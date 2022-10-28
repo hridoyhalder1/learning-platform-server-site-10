9af5342a143e54af68eb5accfa85f16be68cee1b
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-10-28 12:22:04 +0600

    update category_corse

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
 app.use(cors());
 app.get('/', (req, res) => {
@@ -12,6 +13,22 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/course', (req, res) =>{
+    res.send(course);
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_course = course.filter( cc => cc.id === id);
+    res.send(category_course);
+})
+
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find( c => c.id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
